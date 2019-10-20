4145c0d62d2b10a05cbe2488f467af4cff26409f
Glitch (fog-bromine) <none>
2019-10-20 04:17:34 +0000

    👱🖼 Updated with Glitch

--- bot.js
+++ bot.js
@@ -1,67 +1,16 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
+const discordBotkit = require("botkit-discord");
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-			// !vibecheck
-            case 'vibecheck':
-		function randomNumber() {
-    			var res = Math.floor((Math.random() * 10) + 1);
-			}
+const configuration = {
+  token: process.env.DISCORD_TOKEN
+};
 
-			function getRandomSentence() {
-    				var index = Math.floor(Math.random() * (maxSentences - 1));
-    				bot.sendMessage({
-                    			to: channelID,
-                    			message: return sentences[index]
-				}
+const discordBot = discordBotkit(configuration);
+var normalizedPath = require("path").join(__dirname, "skills");
 
-//Random sentences
-		var sentences = [
-    			'vro ur vibes are rancid',
-    			'cold possibly lukewarm',
-    			'perpindicular',
-    			'green',
-    			'good vibes',
-    			'null',
-    			'void'
-		],
-		maxSentences = sentences.length;
-                
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+require("fs")
+  .readdirSync(normalizedPath)
+  .forEach(function(file) {
+    require("./skills/" + file)(discordBot);
+  });
+
+module.exports = discordBot;
